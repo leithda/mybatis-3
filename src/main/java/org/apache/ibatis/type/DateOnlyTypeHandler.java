ce7b2ebd79a5ac7f99cb68a24f1799234e2d41e9
root <leithda@163.com>
2019-08-14 09:06:19 +0800

    类型模块

--- src/main/java/org/apache/ibatis/type/DateOnlyTypeHandler.java
+++ src/main/java/org/apache/ibatis/type/DateOnlyTypeHandler.java
@@ -29,13 +29,16 @@ public class DateOnlyTypeHandler extends BaseTypeHandler<Date> {
   @Override
   public void setNonNullParameter(PreparedStatement ps, int i, Date parameter, JdbcType jdbcType)
       throws SQLException {
+    // 将 java Date 类型抓换为 sql Date 类型
     ps.setDate(i, new java.sql.Date(parameter.getTime()));
   }
 
   @Override
   public Date getNullableResult(ResultSet rs, String columnName)
       throws SQLException {
+    // 获得 sql Date 的值
     java.sql.Date sqlDate = rs.getDate(columnName);
+    // 将 sql Date 转换成 java Date 类型
     if (sqlDate != null) {
       return new Date(sqlDate.getTime());
     }
@@ -45,7 +48,9 @@ public class DateOnlyTypeHandler extends BaseTypeHandler<Date> {
   @Override
   public Date getNullableResult(ResultSet rs, int columnIndex)
       throws SQLException {
+    // 获得 sql Date 的值
     java.sql.Date sqlDate = rs.getDate(columnIndex);
+    // 将 sql Date 转换成 java Date 类型
     if (sqlDate != null) {
       return new Date(sqlDate.getTime());
     }
@@ -55,7 +60,9 @@ public class DateOnlyTypeHandler extends BaseTypeHandler<Date> {
   @Override
   public Date getNullableResult(CallableStatement cs, int columnIndex)
       throws SQLException {
+    // 获得 sql Date 的值
     java.sql.Date sqlDate = cs.getDate(columnIndex);
+    // 将 sql Date 转换成 java Date 类型
     if (sqlDate != null) {
       return new Date(sqlDate.getTime());
     }
